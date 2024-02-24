e603d9f531aabeb326778ee63705243bb62e9abd
Sinval borges neto <Sinval Borges>
2024-02-23 22:06:18 -0300

    colocando opção para pokemon-go

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 AOS.init();
 
+tcgTournament = false;
+goTournament = false;
+var campoAniversario = document.getElementById('birthday-tcg');
+
+
+function registrationControl(tcgTournamentSelect) {
+  var activeGoForm = document.getElementById('go-form');
+  var activeTcgForm = document.getElementById('tcg-form');
+  if (tcgTournamentSelect) {
+    activeTcgForm.style.display = 'flex';
+    activeGoForm.style.display = 'none';
+    campoAniversario = document.getElementById('birthday-tcg');
+    startDate()
+  }
+  if (!tcgTournamentSelect) {
+    activeTcgForm.style.display = 'none';
+    activeGoForm.style.display = 'flex';
+    campoAniversario = document.getElementById('birthday-go');
+    startDate()
+  }
+}
+
 var dataAtual = new Date();
 let ano = dataAtual.getFullYear();
 let mes = dataAtual.getMonth() + 1;
@@ -15,56 +37,60 @@ function obterDataAtual(date) {
   return dataFormatada;
 }
 
-function formaterDataInicial() {
-  return (ano + '-' + (mes < 10 ? '0' : '') + mes + '-' + (dia < 10 ? '0' : '') + dia);
+function startDate() {
+  function formaterDataInicial() {
+    return (ano + '-' + (mes < 10 ? '0' : '') + mes + '-' + (dia < 10 ? '0' : '') + dia);
+  }
+  
+  campoAniversario.max = formaterDataInicial();
+  campoAniversario.value = formaterDataInicial();
+  var oldDate;
+  
+  campoAniversario.addEventListener('change', function() {
+    if (oldDate === undefined) {
+      oldDate = campoAniversario.value
+    }
+    if (oldDate != undefined && campoAniversario.value != '') {
+      oldDate = campoAniversario.value
+    }
+    if (oldDate != undefined && campoAniversario.value === '') {
+      campoAniversario.value = oldDate
+    }
+    if (campoAniversario.value.slice(0, 4) > ano) {
+        campoAniversario.value = ano + campoAniversario.value.substring(4);
+    }
+    if ((campoAniversario.value.slice(5, 7) > mes) && (campoAniversario.value.slice(0, 4) == ano)) {
+      mes = mes*1;
+      mes = (mes < 10 ? '0' : '') + mes;
+      campoAniversario.value = campoAniversario.value.substring(0,5) + mes + campoAniversario.value.substring(7,10);
+    }
+    if ((campoAniversario.value.slice(8, 10) > dia) && (campoAniversario.value.slice(5, 7) == mes) && (campoAniversario.value.slice(0, 4) == ano)) {
+      dia = dia*1;
+      dia = (dia < 10 ? '0' : '') + dia;
+      campoAniversario.value = campoAniversario.value.substring(0,8) + dia;
+    }
+  });
+  
+  campoAniversario.addEventListener('blur', function() {
+    if (campoAniversario.value.slice(0, 4) < 1900) {
+      alert("Por favor, insira um ano válido. Para este formulário deve ser igual ou superior a 1900.");
+      campoAniversario.value = 1900 + campoAniversario.value.substring(4);
+    }
+  });
 }
 
-var campoAniversario = document.getElementById('brithday');
-campoAniversario.max = formaterDataInicial();
-campoAniversario.value = formaterDataInicial();
-var oldDate;
-
-campoAniversario.addEventListener('change', function() {
-  if (oldDate === undefined) {
-    oldDate = campoAniversario.value
-  }
-  if (oldDate != undefined && campoAniversario.value != '') {
-    oldDate = campoAniversario.value
-  }
-  if (oldDate != undefined && campoAniversario.value === '') {
-    campoAniversario.value = oldDate
-  }
-  if (campoAniversario.value.slice(0, 4) > ano) {
-      campoAniversario.value = ano + campoAniversario.value.substring(4);
-  }
-  if ((campoAniversario.value.slice(5, 7) > mes) && (campoAniversario.value.slice(0, 4) == ano)) {
-    mes = mes*1;
-    mes = (mes < 10 ? '0' : '') + mes;
-    campoAniversario.value = campoAniversario.value.substring(0,5) + mes + campoAniversario.value.substring(7,10);
-  }
-  if ((campoAniversario.value.slice(8, 10) > dia) && (campoAniversario.value.slice(5, 7) == mes) && (campoAniversario.value.slice(0, 4) == ano)) {
-    dia = dia*1;
-    dia = (dia < 10 ? '0' : '') + dia;
-    campoAniversario.value = campoAniversario.value.substring(0,8) + dia;
-  }
-});
-
-campoAniversario.addEventListener('blur', function() {
-  if (campoAniversario.value.slice(0, 4) < 1900) {
-    alert("Por favor, insira um ano válido. Para este formulário deve ser igual ou superior a 1900.");
-    campoAniversario.value = 1900 + campoAniversario.value.substring(4);
-  }
-});
 
 function scrollToAbout() {
     var aboutSection = document.getElementById('inscricao');
     aboutSection.scrollIntoView({ behavior: 'smooth' });
   }
 
-  function inscrever() {
-    const nome = document.getElementById('nome').value;
-    const id = document.getElementById('id').value;
-    let nascimento = document.getElementById('brithday').value;
+  function inscrever(tcgForm = true) {
+    const torneio = tcgForm === true ? 'League Challenge de TCG' : 'CUP GO'
+    const tcgOrGO = tcgForm === true ? 'tcg' : 'go'
+    const nome = document.getElementById(`nome-${tcgOrGO}`).value;
+    const id = document.getElementById(`id-${tcgOrGO}`).value;
+    let nascimento = document.getElementById(`birthday-${tcgOrGO}`).value;
     nascimento = obterDataAtual(nascimento);
     if (nome === '' || id === '') {
       let messageAlert = (nome === '' && id === '') ? 'um nome e um id' : nome === '' ? 'um nome' : 'um id'
@@ -72,7 +98,7 @@ function scrollToAbout() {
       return
     }
     
-    const message = `Olá gostaria de realizar a minha inscrição para o torneio.%0A%0AMeus dados são:%0ANome: ${nome}%0AId Pokemon: ${id}%0AData de Nascimento: ${nascimento}`
+    const message = `Olá gostaria de realizar a minha inscrição para o ${torneio}.%0A%0AMeus dados são:%0ANome: ${nome}%0AId Pokemon: ${id}%0AData de Nascimento: ${nascimento}`
     window.open(`https://api.whatsapp.com/send?phone=6282176651&text=${message}`,'_blank')
   }
 
